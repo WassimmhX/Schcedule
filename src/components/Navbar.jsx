46f6dd25feb868bdb4044fbc1dddf2ee383ff231
AhmedMetaoua <ahmedmtawahg@gmail.com>
2025-02-05 00:39:02 +0100

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,7 @@ const Navbar = () => {
       <div className="space-x-4">
         <Link to="/" className="hover:underline">Dashboard</Link>
         <Link to="/planning" className="hover:underline">Planning</Link>
-<<<<<<< HEAD
-        <Link to="/Schcedule" className="hover:underline">Schcedule</Link>
-=======
-        <Link to="/login" className="hover:underline">Login</Link>
-        <Link to="/Schedule" className="hover:underline">Schcedule</Link>
->>>>>>> fc0b5ddead01a53e858180e771881341a454ba8d
+        <Link to="/Schedule" className="hover:underline">Schedule</Link>
         <Link to="/Test" className="hover:underline">Test</Link>
         {isLoggedIn ?<button onClick={()=>logout()} className="text-black font-semibold hover:underline">LogOut</button> 
                     :<Link to="/login" className="hover:underline">Login</Link>}

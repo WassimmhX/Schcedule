b79ccab649b3efa8a5101f508a067572cc6f054b
AhmedMetaoua <ahmedmtawahg@gmail.com>
2025-02-06 23:38:14 +0100

    logo isimm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,8 @@ const Home = () => {
               Login Now
               <LogIn className="ml-2 h-5 w-5" />
             </a>)}
-            <a href={localStorage.getItem('isLogged') ? "/schedules":"/permission-denied"} className="inline-flex items-center justify-center bg-white/10 hover:bg-white/20 backdrop-blur-sm text-white font-semibold py-3 px-7 rounded-lg transition-all">
+            <a href={localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).role == 'admin' ? '/schedules' : "/schedules/"+JSON.parse(localStorage.getItem('user')).role+'s' : "/permission-denied"} 
+                className="inline-flex items-center justify-center bg-white/10 hover:bg-white/20 backdrop-blur-sm text-white font-semibold py-3 px-7 rounded-lg transition-all">
               Explore Schedules
             </a>
           </div>
